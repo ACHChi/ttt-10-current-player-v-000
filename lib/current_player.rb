50a732edd055d72db255d11abfb828a1fb5313bc
Chao Hsuan Chi <allen0413@gmail.com>
2017-10-22 11:19:14 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 def turn_count(board)
   count = 0
-  board.each{
-    |space| if space == "X" || space == "O"
+  board.each{|space| 
+    if space == "X" || space == "O"
       counter += 1
     end
   return counter
