fc279edd7c6e94cb5d5299ce4c326c5224e2c4a3
Chao Hsuan Chi <allen0413@gmail.com>
2017-10-22 11:40:07 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -10,10 +10,16 @@ end
 
 def current_player(board)
 #modular method
-  if turn_count(board) % 2 == 0
+  # if turn_count(board) % 2 == 0
+  #   return "X"
+  # else
+  #   return "O"
+  # end
+
+#even/odd method
+  if turn_count(board).even?
     return "X"
-  else
+  else 
     return "O"
-  end
 
 end
